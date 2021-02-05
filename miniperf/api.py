e8ecc65f51d57bab623050fccf2443c15378e0fc
huanghaiting <290525448@qq.com>
2021-02-05 16:35:17 +0800

    Update api.py

--- miniperf/api.py
+++ miniperf/api.py
@@ -82,37 +82,3 @@ class Api:
 
     def getCurDevice(self):
         return extension.getCurDevice()
-
-    def get_file_list(xlsxpath):
-        file_list = []
-        try:
-            files_status = extension.get_svn_status(xlsxpath)
-            print(files_status)
-
-            files = glob.glob(xlsxpath + '/**/*.xls*', recursive=True)
-            for f in files:
-                if not os.path.basename(f).startswith('~'):
-                    status = files_status.get(f, 'o')
-                    file_list.append({'name': os.path.basename(f), 'path': f, 'status': status})
-
-            
-            # list = output.replace(' ', '')
-            # list = list[2:]
-            # list = list.split(r'\n')        
-            # for f in list:
-            #     name = f[1:-2].split(r'\\')[-1]
-            #     if 'xls' in name and not name.startswith('~'):
-            #         file_list.append({'name': name, 'path': f[1:-2].replace('\\\\','\\'), 'status':f[0]})
-        except Exception as e:
-            print(e)
-            files = glob.glob(xlsxpath + '/**/*.xls*', recursive=True)
-            for f in files:
-                if not os.path.basename(f).startswith('~'):
-                    file_list.append({'name': os.path.basename(f), 'path': f, 'status':'o'})
-        # print(xlsxpath)
-        # files = glob.glob(xlsxpath + '/**/*.xls*', recursive=True)
-        # for f in files:
-        #     if not os.path.basename(f).startswith('~'):
-        #             file_list.append({'name': os.path.basename(f), 'path': f})
-        print(file_list)
-        return file_list
