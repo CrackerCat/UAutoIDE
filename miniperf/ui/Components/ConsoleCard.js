58c95d1a87f50b5dde4e2bea297717db557765b8
黄政杰 <huangzhengjie@kingsoft.com>
2021-04-16 17:44:35 +0800

    fix: 优化ConsoleCard不同信息显示不同颜色的处理方式，禁止ConsoleCard内容拖放。

--- miniperf/ui/Components/ConsoleCard.js
+++ miniperf/ui/Components/ConsoleCard.js
@@ -107,17 +107,24 @@ const ConsoleContent = React.forwardRef((props,ref)=>{
         const testError = new RegExp(/^(\[ERROR\]).*/)
         window.pywebview.api.PythonOutput().then((res)=>{
             if(res !== "405null") {
-                const ret = res.split('\n')
-                const list = ret.map(r => {
-                    if(testInfo.test(r.trim())) return (`<p style="margin: 3px 0; padding: 0; color: #fff; font-size: 12px; font-weight: 400;">${r}</p>`)
-                    if(testError.test(r.trim())) return (`<p style="margin: 3px 0; padding: 0; color: #FF4D4F;  font-size: 12px; font-weight: 400;">${r}</p>`)
-                    return `<p style="margin: 3px 0; padding: 0; color: #fff; font-size: 12px; font-weight: 400;">${r}</p>`
-                })
-                let l = consoleData
-                // l+=(res + '\n')
-                for(let item of list) {
-                    l += item
+                // const ret = res.split('\n')
+                // const list = ret.map(r => {
+                //     if(testInfo.test(r.trim())) return (`<p style="margin: 3px 0; padding: 0; color: #fff; font-size: 12px; font-weight: 400;">${r}</p>`)
+                //     if(testError.test(r.trim())) return (`<p style="margin: 3px 0; padding: 0; color: #FF4D4F;  font-size: 12px; font-weight: 400;">${r}</p>`)
+                //     return `<p style="margin: 3px 0; padding: 0; color: #fff; font-size: 12px; font-weight: 400;">${r}</p>`
+                // })
+                if(testInfo.test(res)) {
+                    res = `<pre style="margin: 0; padding: 0; color: #fff; font-size: 12px; font-weight: 400; line-height: 16px;">${res}</pre>`
+                } else if(testError.test(res)) {
+                    res = `<pre style="margin: 0; padding: 0; color: #FF4D4F;  font-size: 12px; font-weight: 400; line-height: 16px;">${res}</pre>`
+                } else {
+                    res = `<pre style="margin: 0; padding: 0; color: #fff; font-size: 12px; font-weight: 400; line-height: 16px;">${res}</pre>`
                 }
+                let l = consoleData
+                l+=(res + '\n')
+                // for(let item of list) {
+                //     l += item
+                // }
                 onChangeConsoleData(l)
             }
         })
@@ -148,11 +155,13 @@ const ConsoleContent = React.forwardRef((props,ref)=>{
                 className={classes.textArea} 
                 id={'consoleArea'} 
                 contentEditable={true}
+                draggable={false}
                 spellcheck="false"
                 onCut={noop}
                 onCopy={noop}
                 onPaste={noop}
                 onKeyDown={noop}
+                onDragStart={noop}
                 dangerouslySetInnerHTML={{__html: consoleData}}>
             </div>
         </div>
